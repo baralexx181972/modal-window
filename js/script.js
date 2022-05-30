cfad1911a7c0b50f9f72d7f4283a5dbca6f880d4
Vlasenko Aleksandr <baralexx181972@gmail.com>
2022-05-30 11:52:19 +0300

    done

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// --Крутое модальное окно--
+const refs = {
+  openModalBtn: document.querySelector('[data-action = "open-modal"]'),
+  closeModalBtn: document.querySelector('[data-action = "close-modal"]'),
+  backdrop: document.querySelector(".js-backdrop"),
+};
+
+refs.openModalBtn.addEventListener("click", onOpenModal);
+refs.closeModalBtn.addEventListener("click", onCloseModal);
+refs.backdrop.addEventListener("click", onBackDrop);
+// Открытие и закрытие модалки через кнопки
+
+function onOpenModal() {
+  window.addEventListener("keydown", onEscKeyPress);
+  document.body.classList.add("show-modal");
+}
+function onCloseModal() {
+  window.removeEventListener("keydown", onEscKeyPress);
+  document.body.classList.remove("show-modal");
+}
+//  Закрытие модалки по клику на бекдроп
+
+function onBackDrop(event) {
+  if (event.currentTarget === event.target) {
+    onCloseModal();
+  }
+}
+//  Закрытие модалки по клику на Esc
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = "Escape";
+  if (event.code === ESC_KEY_CODE) {
+    onCloseModal();
+  }
+}
